dda4e665c9d610b1f545f0a386b892c6972c0928
Jason Yoon <jasony0206@gmail.com>
2017-01-11 22:09:36 -0800

    Start using fixtures

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -9,3 +9,21 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+class NginxStringConfigParserTest : public ::testing::Test {
+protected:
+    bool ParseString(const std::string config_string) {
+      std::stringstream config_stream(config_string);
+      return parser_.Parse(&config_stream, &out_config_);
+    }
+
+    NginxConfigParser parser_;
+    NginxConfig out_config_;
+};
+
+TEST_F(NginxStringConfigParserTest, UsingFixture) {
+  std::string config_string = "foo bar;";
+  EXPECT_TRUE(ParseString(config_string));
+  EXPECT_EQ("foo", out_config_.statements_[0]->tokens_[0]);
+  EXPECT_EQ("bar", out_config_.statements_[0]->tokens_[1]);
+}
